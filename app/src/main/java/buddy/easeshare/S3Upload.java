4681ad74d288a83b4f0109b2b28b2f3d79979689
Dharmendra Nasit <dharmendrabuddy@gmail.com>
2018-10-18 20:16:30 +0530

    Detect (Partial )

--- app/src/main/java/buddy/easeshare/S3Upload.java
+++ app/src/main/java/buddy/easeshare/S3Upload.java
@@ -1,12 +1,23 @@
 package buddy.easeshare;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
@@ -32,8 +43,17 @@ import com.amazonaws.services.rekognition.model.SearchFacesByImageRequest;
 import com.amazonaws.services.rekognition.model.SearchFacesByImageResult;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.squareup.picasso.Picasso;
+//import com.bumptech.glide.Glide;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,6 +65,9 @@ public class S3Upload {
     private CognitoUserSession cognitoUserSession;
     private Regions regions = Regions.US_EAST_1;
     private MainActivity mainActivity;
+    private URL url;
+    ArrayList<String> list =new ArrayList<String>();
+
 
     public void upload(CognitoUser cognitoUser, Context context, Bitmap bitmap,MainActivity mainActivity) {
         this.context=context;
@@ -107,14 +130,29 @@ public class S3Upload {
         }
     };
 
-    private class Upload extends AsyncTask<CognitoCachingCredentialsProvider, Void, String> {
+    private class Upload extends AsyncTask<CognitoCachingCredentialsProvider, Void, ArrayList> {
 
         @Override
-        protected String doInBackground(CognitoCachingCredentialsProvider... params) {
+        protected ArrayList doInBackground(CognitoCachingCredentialsProvider... params) {
 
+
+
+
+
+            AmazonS3 s3 = new AmazonS3Client(cognitoCachingCredentialsProvider);
+            java.util.Date expiration = new java.util.Date();
+            long expTimeMillis = expiration.getTime();
+            expTimeMillis += 1000 * 60 * 60;
+            expiration.setTime(expTimeMillis);
+            GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                    new GeneratePresignedUrlRequest("easeshare", "p3.jpg")
+                            .withMethod(HttpMethod.GET)
+                            .withExpiration(expiration);
+            url = s3.generatePresignedUrl(generatePresignedUrlRequest);
+            list.add(url.toString());
 
             AmazonRekognition rekognitionClient = new  AmazonRekognitionClient(cognitoCachingCredentialsProvider);
-            String photo = "download.jpg";
+            String photo = "p3.jpg";
             //ObjectMapper objectMapper = new ObjectMapper();
 
             // Get an image object from S3 bucket.
@@ -123,22 +161,43 @@ public class S3Upload {
                             .withBucket("easeshare")
                             .withName(photo));
 
+
             // Search collection for faces similar to the largest face in the image.
             SearchFacesByImageRequest searchFacesByImageRequest = new SearchFacesByImageRequest()
                     .withCollectionId("EaseShare")
                     .withImage(image)
                     .withFaceMatchThreshold(70F)
-                    .withMaxFaces(2);
+                    .withMaxFaces(1);
 
             SearchFacesByImageResult searchFacesByImageResult =
                     rekognitionClient.searchFacesByImage(searchFacesByImageRequest);
+
+
 
             System.out.println("Faces matching largest face in image from" + photo);
             List <FaceMatch> faceImageMatches = searchFacesByImageResult.getFaceMatches();
             for (FaceMatch face: faceImageMatches) {
                 //System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(face));
                 Log.d("Hello",face.getFace().getBoundingBox().toString());
+                list.add(face.getFace().getBoundingBox().toString());
                 System.out.println();
+            }
+
+            try {
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                myBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] b = baos.toByteArray();
+                String temp = Base64.encodeToString(b, Base64.DEFAULT);
+                list.add(temp);
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
             /**AmazonRekognition rekognitionClient = new  AmazonRekognitionClient(cognitoCachingCredentialsProvider);
@@ -178,6 +237,8 @@ public class S3Upload {
             Log.d("HEllo",createCollectionResult.getCollectionArn());
             //System.out.println("CollectionArn : " + createCollectionResult.getCollectionArn());
             //System.out.println("Status code : " + createCollectionResult.getStatusCode().toString());**/
+
+
             /**AmazonS3 s3 =new AmazonS3Client(cognitoCachingCredentialsProvider);
             try {
                 s3.deleteBucket("dharam-bhaumik");
@@ -185,14 +246,62 @@ public class S3Upload {
                 System.err.println(e.getErrorMessage());
                 System.exit(1);
             }**/
-            return "Deleted";
+            return list;
         }
 
         @Override
-        protected void onPostExecute(String result) {
+        protected void onPostExecute(ArrayList result) {
                 ImageView imageView = mainActivity.findViewById(R.id.imageView);
-                imageView.setImageBitmap(bitmap);
-                new MainActivity().response(result);
+                //ArrayList<String> res = new ArrayList<String>(result);
+                String params = result.get(1).toString();
+                String[] arr = params.split(",");
+                float fwidth = Float.parseFloat(arr[0].split(":")[1].trim());
+                float fheight = Float.parseFloat(arr[1].split(":")[1].trim());
+                float fleft = Float.parseFloat(arr[2].split(":")[1].trim());
+                float ftop = Float.parseFloat(arr[3].split(":")[1].trim().substring(0,7));
+
+
+
+
+
+            Paint p = new Paint();
+            p.setColor(Color.BLACK);
+            p.setStyle(Paint.Style.STROKE);
+            p.setStrokeWidth(5);
+
+            byte[] encodeByte = Base64.decode(result.get(2).toString(), Base64.DEFAULT);
+            Bitmap b = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+
+            int imgWidth = b.getWidth();
+            int imgHeight = b.getHeight();
+
+            Log.d("Hello",imgWidth + " :: " + imgHeight);
+            Log.d("Hello",b.getWidth() + " :: " + b.getHeight());
+
+            float top = ftop * (float) imgHeight;
+            float left = fleft * (float) imgWidth;
+            float height = fheight * (float) imgHeight;
+            float width = fwidth * (float) imgWidth;
+
+            Log.d("Hello", "top : "+ top + "left : " + left + "height : " + height + "width : " +width);
+
+            Bitmap tBitmap = Bitmap.createBitmap(imgWidth, imgHeight, Bitmap.Config.RGB_565);
+
+            Canvas tCanvas = new Canvas(tBitmap);
+
+            Log.d("Hello",tCanvas.getWidth()+" :: " + tCanvas.getHeight());
+
+            tCanvas.drawBitmap(b, 0, 0, null);
+
+            tCanvas.drawRoundRect(new RectF(left,top,left+width,top+height), 2, 2, p);
+
+            imageView.setImageDrawable(new BitmapDrawable(mainActivity.getResources(), tBitmap));
+
+
+                //String url = result.get(0).toString();
+                //Picasso.get().load(url).into(imageView);
+                //imageView.setImageBitmap(bitmap);
+                //new MainActivity().response(result);
         }
 
         @Override
